9a024421262ca2062dd63f1f7680cb0b15b7bd45
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2018-01-27 13:12:22 -0500

    Add ContentController.cs in the Controllers folder

---
+++ ApiControllers/Controllers/ContentController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using ApiControllers.Models;
+
+namespace ApiControllers.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContentController : Controller 
+    {
+        [HttpGet("string")]
+        public string GetString() => "This is a string response";
+
+        [HttpGet("object")]
+        public Reservation GetObject() => new Reservation {
+            ReservationId = 100,
+            ClientName = "Joe",
+            Location = "Board Room"
+        };
+    }
+}
